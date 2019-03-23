b8391561429b8799cba9bb4a695b3fbef0697f66
Palash A <palash.agarwal.96@gmail.com>
2019-03-24 03:53:26 +0530

    shortest job first

--- 06/sjf.c
+++ 06/sjf.c
@@ -1,6 +1,19 @@
 // Shortest Job First
 #include "consts.h"
+#include <stdlib.h>
 
-double sjf(int *p, int n) {
-  return 0.0;
+int shortest_burst(const void *a, const void *b) {
+  Process *A = (Process *) a;
+  Process *B = (Process *) b;
+  return (A->burst - B->burst);
+}
+
+double sjf(Process *p, int n) {
+  int time = 0, wait = 0;
+  qsort(p, n, sizeof(Process), shortest_burst);
+  for(int i=0; i<n; i++) {
+    wait += time - p[i].arrival;
+    time += p[i].burst;
+  }
+  return (wait / (double) n);
 }
