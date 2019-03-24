5d9d3860b6cd9556f4e262306586449f8e2fe603
Palash A <palash.agarwal.96@gmail.com>
2019-03-24 06:59:23 +0530

    round robin

--- 06/consts.h
+++ 06/consts.h
@@ -1,10 +1,13 @@
+#define QUANTUM 1
+
 #ifndef PROCESS
 #define PROCESS
-
 typedef struct {
   int burst;
   int arrival;
   int priority;
 } Process;
-
 #endif
+
+// qsort comparators
+int priority_first(const void *a, const void *b);
--- 06/rr.c
+++ 06/rr.c
@@ -1,6 +1,20 @@
-// Round Robin Scheduling Algorithm
 #include "consts.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-double rr(int* p, int n) {
-  return 0.0;
+double rr(Process* p, int n) {
+  qsort(p, n, sizeof(Process), priority_first);
+  printf("Using time quantum %d\n", QUANTUM);
+  int wait = 0, index = 0, left = n, q = QUANTUM;
+  while (left != 0) {
+    if (p[index].burst < QUANTUM)
+      q = p[index].burst;
+    p[index].burst -= q;
+    wait += q * (left-1); // running process is not waiting
+    if (p[index].burst == 0)
+      left--;
+    if (++index == n) 
+      index = 0;
+  }
+  return (wait / (double) n);
 }
