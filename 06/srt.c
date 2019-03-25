d9a9dcf3d8ceb47cafa13ed0fe7bdfbfe7d85d61
Palash A <palash.agarwal.96@gmail.com>
2019-03-25 10:56:55 +0530

    shortest remaining time

--- 06/srt.c
+++ 06/srt.c
@@ -1,6 +1,11 @@
-// Shortest Remaining Time
 #include "consts.h"
 
-double srt(int *p, int n) {
-  return 0.0;
+double srt(Process *p, int n) {
+  qsort(p, n, sizeof(Process), shortest_job_first);
+  int wait = 0, time = 0;
+  for(int i=0; i<n; i++) {
+    wait += time - p[i].arrival;
+    time += p[i].burst;
+  } // Same as SJF lol; No alteration inbetween execution
+  return (wait / (double) n);
 }
